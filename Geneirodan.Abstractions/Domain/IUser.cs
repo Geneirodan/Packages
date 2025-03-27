b117739a5b8c6c51261a2d4f8b4b5de71b892f87
Al'ciel Geneirodan <geneirodanjekin@gmail.com>
2025-03-27 23:37:35 +0100

    Added Geneirodan.Abstractions package

---
+++ Geneirodan.Abstractions/Domain/IUser.cs
@@ -0,0 +1,20 @@
+namespace Geneirodan.Abstractions.Domain;
+
+/// <summary>
+/// Represents a user in the system with basic properties and capabilities, such as the user's identifier and role-based access.
+/// </summary>
+public interface IUser
+{
+    /// <summary>
+    /// Unique identifier of the user.
+    /// The identifier is nullable in case the user is not yet assigned an ID (e.g., for anonymous users).
+    /// </summary>
+    Guid? Id { get; }
+
+    /// <summary>
+    /// Determines whether the user is in the specified role.
+    /// </summary>
+    /// <param name="role">The name of the role to check against the user's roles.</param>
+    /// <returns>True if the user is in the specified role; otherwise, false.</returns>
+    bool IsInRole(string role);
+}
