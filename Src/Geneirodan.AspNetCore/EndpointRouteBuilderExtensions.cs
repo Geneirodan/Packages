a3db6c49adbef9338759269ad791be74bfe53a7a
Al'ciel Geneirodan <geneirodanjekin@gmail.com>
2025-04-25 15:44:01 +0200

    Remove GitVersion.MsBuild references and add health check endpoint mapping

---
+++ Src/Geneirodan.AspNetCore/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HealthChecks.UI.Client;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Routing;
+
+namespace Geneirodan.AspNetCore;
+
+/// <summary>
+/// Provides extension methods for <see cref="IEndpointRouteBuilder"/> to map health check endpoints
+/// with a default or specified route pattern and response writer.
+/// </summary>
+[PublicAPI]
+public static class EndpointRouteBuilderExtensions
+{
+    /// <summary>
+    /// Maps a health check endpoint to the specified route in the <see cref="IEndpointRouteBuilder"/>.
+    /// </summary>
+    /// <param name="routeBuilder">The endpoint route builder to map the health check endpoint to.</param>
+    /// <param name="pattern"></param>
+    /// <returns>An <see cref="IEndpointConventionBuilder"/> for the mapped health check endpoint.</returns>
+    public static IEndpointConventionBuilder MapHealthChecks(
+        this IEndpointRouteBuilder routeBuilder,
+        [StringSyntax("Route")]string pattern = "/health"
+    ) => routeBuilder.MapHealthChecks(
+        pattern: pattern,
+        options: new HealthCheckOptions { ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponseNoExceptionDetails }
+    );
+    
+}
