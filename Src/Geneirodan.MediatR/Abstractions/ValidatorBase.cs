d7a9ce637fadec1bf46a448bf0cc20d44c05b2fd
Al'ciel Geneirodan <geneirodanjekin@gmail.com>
2025-05-15 17:47:56 +0200

    Add abstract Validator class for FluentValidation integration

---
+++ Src/Geneirodan.MediatR/Abstractions/ValidatorBase.cs
@@ -0,0 +1,8 @@
+﻿using FluentValidation;
+using JetBrains.Annotations;
+
+namespace Geneirodan.MediatR.Abstractions;
+
+/// <inheritdoc/>
+[UsedImplicitly(ImplicitUseTargetFlags.WithInheritors)]
+public abstract class ValidatorBase<T> : AbstractValidator<T>;
