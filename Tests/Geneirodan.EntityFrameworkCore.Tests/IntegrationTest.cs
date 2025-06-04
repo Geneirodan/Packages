6a22e7884ea70330ac418bdd6ed62b7be459df34
Al'ciel Geneirodan <geneirodanjekin@gmail.com>
2025-06-05 01:08:24 +0200

    Add ITransaction and IUnitOfWork interfaces and corresponding implementations for EF Core

---
+++ Tests/Geneirodan.EntityFrameworkCore.Tests/IntegrationTest.cs
@@ -0,0 +1,28 @@
+﻿using Geneirodan.Abstractions.Repositories;
+using Geneirodan.SampleApi;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Geneirodan.EntityFrameworkCore.Tests;
+
+public abstract class IntegrationTest : IClassFixture<ApiFactory>, IDisposable
+{
+    protected IntegrationTest(ApiFactory factory)
+    {
+        Factory = factory;
+        Scope = factory.Services.CreateScope();
+        Context = Scope.ServiceProvider.GetRequiredService<DbContext>();
+    }
+
+    protected readonly WebApplicationFactory<IApiMarker> Factory;
+    protected IServiceScope Scope;
+    protected DbContext Context;
+
+    public void Dispose()
+    {
+        Scope.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
